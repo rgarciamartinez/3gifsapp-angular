f1aeb457404ac606c732bf5d0931fc8bcb7407fb
RGM <robin13752@gmail.com>
2025-11-06 21:47:37 -0600

    update 21:40 6/11/25

--- src/app/gifs/components/sideMenuOptions/sideMenuOptions.ts
+++ src/app/gifs/components/sideMenuOptions/sideMenuOptions.ts
@@ -1,9 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
+interface menuOption{
+  label:string,
+  subLabel:string,
+  route:string,
+  icon:string
+}
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sideMenuOptions.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+
+  menuOptions:menuOption[] = [
+    {
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    subLabel: 'Trending Gifts',
+    route: '/dashboard/trending',
+  },
+    {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Search',
+    subLabel: 'Search Gifts',
+    route: '/dashboard/search',
+  },
+]
+
+}
